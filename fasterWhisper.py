0f342b3eaff36983452390484c95f7f0f39c58ba
Nikhil Kumar <nikhilkapooriitk@gmail.com>
2024-12-16 00:24:25 +0530

    This is fasterWhisper

---
+++ fasterWhisper.py
@@ -0,0 +1,20 @@
+import os
+os.environ["KMP_DUPLICATE_LIB_OK"]="TRUE"
+from faster_whisper import WhisperModel
+
+model_size = "large-v3"
+
+# Run on GPU with FP16
+model = WhisperModel(model_size, device="cuda", compute_type="int8")
+
+# or run on GPU with INT8
+# model = WhisperModel(model_size, device="cuda", compute_type="int8_float16")
+# or run on CPU with INT8
+# model = WhisperModel(model_size, device="cpu", compute_type="int8")
+
+segments, info = model.transcribe("tts.mp3", beam_size=5)
+
+print("Detected language '%s' with probability %f" % (info.language, info.language_probability))
+
+for segment in segments:
+    print("[%.2fs -> %.2fs] %s" % (segment.start, segment.end, segment.text))
